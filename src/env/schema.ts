fe59b5ff609ba13a6e856b78661b8b96215dc904
Xarib <xarib@pm.me>
2023-02-26 20:14:15 +0100

    fix eslint, add prettier

--- src/env/schema.ts
+++ src/env/schema.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 
 export const serverScheme = z.object({
-  NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
   GITHUB_ID: z.string(),
   GITHUB_SECRET: z.string(),
   AUTH_SECRET: z.string(),
@@ -11,5 +13,5 @@ export const serverScheme = z.object({
 });
 
 export const clientScheme = z.object({
-  MODE: z.enum(['development', 'production', 'test']).default('development'),
+  MODE: z.enum(["development", "production", "test"]).default("development"),
 });
