7827ce0c25b52dadc96b34c4638fb9727288ea65
Xarib <xarib@pm.me>
2023-03-11 21:37:30 +0100

    Add unplugin icons

--- src/routes/testgrounds/icon-buttons.tsx
+++ src/routes/testgrounds/icon-buttons.tsx
@@ -3,6 +3,8 @@ import { AnchorButton } from "~/components/button/AnchorButton";
 import { AnchorIconButton } from "~/components/icon-button/anchor-icon-button";
 import IconButton from "~/components/icon-button/icon-button";
 import WidthConstraint from "~/components/width-constraint";
+import { ICON_MEDIUM } from "~/utility/iconSize";
+import PhMegaphoneSimpleDuotone from "~icons/ph/megaphone-simple-duotone";
 
 const IconButtons: VoidComponent = () => {
   return (
@@ -11,14 +13,18 @@ const IconButtons: VoidComponent = () => {
         <AnchorButton href="../">Back</AnchorButton>
         <div>
           <h1>Anchorbuttons</h1>
-          <AnchorIconButton href="" hoverTitle="tag">
-            Icon
+          <AnchorIconButton
+            href="https://www.solidjs.com"
+            target="_blank"
+            hoverTitle="tag"
+          >
+            <PhMegaphoneSimpleDuotone class={ICON_MEDIUM} />
           </AnchorIconButton>
         </div>
         <div>
           <h1>Click</h1>
           <IconButton onClick={() => alert("click")} hoverTitle="tag">
-            Icon
+            <PhMegaphoneSimpleDuotone class={ICON_MEDIUM} />
           </IconButton>
         </div>
       </WidthConstraint>
